4eb54508c29c7a77bf1db83e99c72c5bd5918618
SICOT Jean Vivien <jv.sicot@gt-logistics.fr>
2018-12-03 15:43:04 +0100

    modif = adminUser

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -9,12 +9,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 /**
  * @Route("/utilisateur")
  */
 class UtilisateurController extends AbstractController
 {
+
     /**
      * @Route("/", name="utilisateur_index", methods="GET")
      */
@@ -22,6 +32,131 @@ class UtilisateurController extends AbstractController
     {
         return $this->render('utilisateur/index.html.twig', ['utilisateurs' => $utilisateurRepository->findAll()]);
     }
+
+    /**
+    * @Route ("/admin", name="admin")
+    */
+    public function adminUtilisateur(UtilisateurRepository $utilisateurRepository,  UserPasswordEncoderInterface $passwordEncoder, Request $request): Response
+    {
+
+        if ($request->isXmlHttpRequest()) {
+
+            $current = $request->request->get('current');
+            $rowCount = $request->request->get('rowCount');
+            $searchPhrase = $request->request->get('searchPhrase');
+            $sort = $request->request->get('sort');
+
+            $utilisateurs = $utilisateursRepository->findBySearchSort($searchPhrase, $sort);
+
+            if ($searchPhrase != "") {
+                $count = count($utilisateurs->getQuery()->getResult());
+            } else {
+                $count = count($utilisateursRepository->findAll());
+            }
+
+            if ($rowCount != -1) {
+                $min = ($current - 1) * $rowCount;
+                $max = $rowCount;
+
+                $utilisateurs->setMaxResults($max)
+                    ->setFirstResult($min);
+            }
+
+            $utilisateurs = $utilisateurs->getQuery()->getResult();
+
+            $rows = array();
+            foreach ($utilisateurs as $utilisateur) {
+                $roles = $utilisateur->getRoles();
+                $roles_string = "";
+                foreach ($roles as $role) {
+                    $roles_string = $role . ", " . $roles_string;
+                }
+
+                // enlève les deux derniers caractères
+                $roles_string = substr($roles_string, 0, -2);
+
+
+                // format de la derniere date de connexion
+                if ($utilisateur->getLastLogin()) {
+                    $lastLogin = date_diff(new \Datetime(), $utilisateur->getLastLogin());
+
+                    $format = "Il y a ";
+                    if ($lastLogin->y) {
+                        $format = $format . "environ " . $lastLogin->y . "an(s) " . $lastLogin->m . "mois";
+                    } else if ($lastLogin->m) {
+                        $format = $format . "environ " . $lastLogin->m . "mois " . $lastLogin->d . "jour(s)";
+                    } else if ($lastLogin->d) {
+                        $format = $format . $lastLogin->d . "jour(s) " . $lastLogin->h . "heure(s)";
+                    } else if ($lastLogin->h) {
+                        $format = $format . $lastLogin->h . "h" . $lastLogin->i . "min";
+                    } else {
+                        $format = $format . $lastLogin->i . "min";
+                    }
+
+                    $lastLogin = $lastLogin->format($format);
+
+                } else {
+                    $lastLogin = "Aucune connexion";
+                }
+
+
+                $row = [
+                    "id" => $utilisateur->getId(),
+                    "username" => $utilisateur->getUsername(),
+                    "email" => $utilisateur->getEmail(),
+                    "groupe" => $utilisateur->getGroupe(),
+                    "lastLogin" => $lastLogin,
+                    "roles" => $roles_string,
+                ];
+
+                array_push($rows, $row);
+            }
+
+            $data = array(
+                "current" => intval($current),
+                "rowCount" => intval($rowCount),
+                "rows" => $rows,
+                "total" => intval($count)
+            );
+
+            return new JsonResponse($data);
+        } 
+
+        $user = new Utilisateur();
+        $form_creation = $this->createForm(UtilisateurType::class, $user);
+
+        $form_modif = $this->createFormBuilder()
+        ->add('id_user', HiddenType::class, array(
+            'mapped' => false,
+        ))
+        ->add('email', EmailType::class, array(
+            'label' => "Adresse email"
+        ))
+        ->add('username', TextType::class, array(
+            'label' => "Nom d'utilisateur"
+        ))
+        ->add('plainPassword', PasswordType::class, array(
+            'label' => "Réinitialiser Mot de Passe",
+        ))
+        ->add('roles', ChoiceType::class, array(
+            'label' => 'Rôles',
+            'choices' => array(
+                'Utilisateur' => 'ROLE_USER',
+                'Utilisateur parc' => 'ROLE_PARC',
+                'Admin parc' => 'ROLE_PARC_ADMIN',
+            ),
+            'multiple' => true,
+        ))
+        ->getForm();
+
+        return $this->render("security/utilisateurs.html.twig", array(
+            'utilisateurs' => $utilisateurRepository->findAll(),
+            'form_creation' => $form_creation->createView(),
+            'form_modif' => $form_modif->createView()
+
+        ));
+    }
+
 
     /**
      * @Route("/new", name="utilisateur_new", methods="GET|POST")
@@ -87,4 +222,6 @@ class UtilisateurController extends AbstractController
 
         return $this->redirectToRoute('utilisateur_index');
     }
+
+   
 }
--- src/Repository/UtilisateurRepository.php
+++ src/Repository/UtilisateurRepository.php
@@ -20,6 +20,36 @@ class UtilisateurRepository extends ServiceEntityRepository
     }
 
     // /**
+    //  * @return Parcs[] Returns an array of Parcs objects
+    //  */
+    // public function findBySearchSort($searchPhrase, $sort)
+    // {
+    //     $qb = $this->createQueryBuilder('user');
+    //     $parameters = [];
+    //     $key_id = 0;
+
+    //     if ($searchPhrase != "") {
+    //         $qb->leftJoin('user.groupe', 'groupe')
+    //             ->andWhere('user.username LIKE :search
+    //             OR user.email LIKE :search
+    //             OR groupe.nom LIKE :search
+    //             OR user.roles LIKE :search
+    //             OR user.lastLogin LIKE :search
+    //         ')
+    //             ->setParameter('search', '%' . $searchPhrase . '%');
+    //     }
+
+    //     if ($sort) {
+    //         foreach ($sort as $key => $value) {
+    //             $qb->orderBy('user.' . $key, $value);
+    //         }
+    //     } else {
+    //         $qb->orderBy('user.lastLogin', 'ASC');
+    //     }
+
+    //     return $qb;
+    // }
+    // /**
     //  * @return Utilisateur[] Returns an array of Utilisateur objects
     //  */
     /*
